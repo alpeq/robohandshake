4d203e8c32c40cdb450798a1f920339a97e01061
apequeno <alejapeq@gmail.com>
2024-05-15 11:17:29 +0200

    Motor and sensor integrated in observer classes First handshake protocol

--- pyscripts/handshake_protocol.py
+++ pyscripts/handshake_protocol.py
@@ -1,140 +1,28 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+from helpers.classes import *
 
-import os
-import sys, tty, termios
-from dynamixel_sdk import * # Uses Dynamixel SDK library
-from abc import ABC, abstractmethod
-
-fd = sys.stdin.fileno()
-old_settings = termios.tcgetattr(fd)
-#********* DYNAMIXEL Model definition *********
-MY_DXL = 'X_SERIES'       # X430 
-
-# Control table address
-if MY_DXL == 'X_SERIES':
-    ADDR_TORQUE_ENABLE          = 64
-    ADDR_GOAL_POSITION          = 116
-    ADDR_PRESENT_POSITION       = 132
-    ADDR_PROFILE_ACCELERATION	= 108
-    ADDR_PROFILE_VELOCITY		= 112
-    DXL_MINIMUM_POSITION_VALUE  = 0         # Refer to the Minimum Position Limit of product eManual
-    DXL_MAXIMUM_POSITION_VALUE  = 4090      # Refer to the Maximum Position Limit of product eManual
-    BAUDRATE                    = 1000000
-
-PROTOCOL_VERSION            = 2.0
-DXL_ID                      = 1
-DEVICENAME                  = '/dev/ttyUSB0'
-
-TORQUE_ENABLE               = 1     # Value for enabling the torque
-TORQUE_DISABLE              = 0     # Value for disabling the torque
-DXL_MOVING_STATUS_THRESHOLD = 20    # Dynamixel moving status threshold
-
-dxl_goal_position = [1200, 1600]         # Goal position
-
-
-def getch():
-    try:
-        tty.setraw(sys.stdin.fileno())
-        ch = sys.stdin.read(1)
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    return ch
-
-def setup_motor():
-    # Initialize PortHandler and PacketHandler instance
-    portHandler = PortHandler(DEVICENAME)
-    packetHandler = PacketHandler(PROTOCOL_VERSION)
-    
-    # Open port
-    if portHandler.openPort():
-        print("Succeeded to open the port")
-    else:
-        print("Failed to open the port")
-        print("Press any key to terminate...")
-        getch()
-        quit()
-    
-    # Set port baudrate
-    if portHandler.setBaudRate(BAUDRATE):
-        print("Succeeded to change the baudrate")
-    else:
-        print("Failed to change the baudrate")
-        print("Press any key to terminate...")
-        getch()
-        quit()
-    
-    # Enable Dynamixel Torque
-    dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_TORQUE_ENABLE, TORQUE_ENABLE)
-    if dxl_comm_result != COMM_SUCCESS:
-        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
-        print("SUCCESS")
-    elif dxl_error != 0:
-        print("%s" % packetHandler.getRxPacketError(dxl_error))
-    else:
-        print("Dynamixel has been successfully connected")
-    # Acceleration and velocity user friendly profile
-    dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, ID, ADDR_PROFILE_ACCELERATION, 3)
-    if dxl_comm_result != COMM_SUCCESS:
-        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
-    elif dxl_error != 0:
-        print("%s" % packetHandler.getRxPacketError(dxl_error))
-    dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, ID, ADDR_PROFILE_VELOCITY, 30)
-    if dxl_comm_result != COMM_SUCCESS:
-        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
-    elif dxl_error != 0:
-        print("%s" % packetHandler.getRxPacketError(dxl_error))
-
-
-def cleanup_motor():
-    # Disable Dynamixel Torque
-    dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_TORQUE_ENABLE, TORQUE_DISABLE)
-    if dxl_comm_result != COMM_SUCCESS:
-        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
-    elif dxl_error != 0:
-        print("%s" % packetHandler.getRxPacketError(dxl_error))
-    
-    # Close port
-    portHandler.closePort()
+Closed_goal = 1200
+Open_goal = 1600
 
 
 def main():
-''' Handshake Protocol '''
-
-    setup_motor()
-
+    ''' Handshake Protocol '''
     while 1:
         print("Press any key to start the protocol! (or press ESC to quit!)")
         if getch() == chr(0x1b):
             break
+        # The client code.
+
+        handsense_topic = SensorStatus()
+        handaction_sub = MotorClamp(Closed_goal, Open_goal)
+        handsense_topic.attach(handaction_sub)
+
+        # handaction --> Open clamp and wait for sensor input
+        handsense_topic.read_sensor_logic()
+        # handaction --> close clamp and shake until intense pressure
+        handsense_topic.read_sensor_logic()
+        # handaction --> open clamp and feedback audio?
     
-        # Write goal position
-        dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, DXL_ID, ADDR_GOAL_POSITION, dxl_goal_position[index])
-        if dxl_comm_result != COMM_SUCCESS:
-            print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
-        elif dxl_error != 0:
-            print("%s" % packetHandler.getRxPacketError(dxl_error))
-    
-        while 1:
-            # Read present position
-            dxl_present_position, dxl_comm_result, dxl_error = packetHandler.read4ByteTxRx(portHandler, DXL_ID, ADDR_PRESENT_POSITION)
-            if dxl_comm_result != COMM_SUCCESS:
-                print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
-            elif dxl_error != 0:
-                print("%s" % packetHandler.getRxPacketError(dxl_error))
-    
-            print("[ID:%03d] GoalPos:%03d  PresPos:%03d" % (DXL_ID, dxl_goal_position[index], dxl_present_position))
-    
-            if abs(dxl_goal_position[index] - dxl_present_position) <= DXL_MOVING_STATUS_THRESHOLD:
-                break
-    
-        # Change goal position
-        if index == 0:
-            index = 1
-        else:
-            index = 0
-    
-    clean_up_motor()
+    handaction_sub.cleanup_motor()
 
 
 if __name__ == '__main__':
--- pyscripts/helpers/classes.py
+++ pyscripts/helpers/classes.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from __future__ import annotations
 from abc import ABC, abstractmethod
 from random import randrange
@@ -8,6 +9,55 @@ import json
 import Adafruit_BBIO.ADC as ADC
 import time
 
+## Motor
+
+import os
+
+if os.name == 'nt':
+    import msvcrt
+    def getch():
+        return msvcrt.getch().decode()
+else:
+    import sys, tty, termios
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    def getch():
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+import sys, tty, termios
+from dynamixel_sdk import * # Uses Dynamixel SDK library
+from abc import ABC, abstractmethod
+
+fd = sys.stdin.fileno()
+old_settings = termios.tcgetattr(fd)
+#********* DYNAMIXEL Model definition *********
+MY_DXL = 'X_SERIES'       # X430
+
+# Control table address
+if MY_DXL == 'X_SERIES':
+    ADDR_TORQUE_ENABLE          = 64
+    ADDR_GOAL_POSITION          = 116
+    ADDR_PRESENT_POSITION       = 132
+    ADDR_PROFILE_ACCELERATION	= 108
+    ADDR_PROFILE_VELOCITY		= 112
+    DXL_MINIMUM_POSITION_VALUE  = 0         # Refer to the Minimum Position Limit of product eManual
+    DXL_MAXIMUM_POSITION_VALUE  = 4090      # Refer to the Maximum Position Limit of product eManual
+    BAUDRATE                    = 1000000
+
+PROTOCOL_VERSION            = 2.0
+DXL_ID                      = 1
+DEVICENAME                  = '/dev/ttyUSB0'
+
+TORQUE_ENABLE               = 1     # Value for enabling the torque
+TORQUE_DISABLE              = 0     # Value for disabling the torque
+DXL_MOVING_STATUS_THRESHOLD = 20    # Dynamixel moving status threshold
+
+
+## Sensor
 ADC.setup()
 
 Thumb = 0
@@ -65,7 +115,6 @@ class SensorStatus(Subject):
     """
     SensorStatus is a Subject node that notifies the status of each of the sensors for motor control and logging
     """
-
     def __init__(self, file_name):
         self._state: List[int] = [0,0,0]    # int state of each of the sensors [0] neutral - [1] touched -  [2] intense pressure
         self._observers: List[Observer] = []
@@ -89,6 +138,7 @@ class SensorStatus(Subject):
         """
         Loop to read sensors
         """
+        break_flag = False
         with open(self.fname, 'a') as file:
             while True:
                 self._state, dict_out = read_pins()
@@ -96,20 +146,111 @@ class SensorStatus(Subject):
                 file.write(out_dump)
                 for sensor in self._state:
                     if sensor > 25:
-                        break
+                        break_flag = True
                 # print(out_dump)
+                if break_flag:
+                    break
                 time.sleep(0.1)
-
         self.notify()
 
 class MotorClamp(Observer):
-    def __init__(self):
-        self.motor_handle = None
+    def __init__(self, goal_open, goal_closed):
+        #self.motor_handle = None
+        self.port_handler, self.packet_handler =  self.self.setup_motor()
+        self.open = goal_open
+        self.close = goal_closed
+        self.flag_change = False
     def update(self, subject: Subject) -> None:
         print("Thumb state has changed to: {}".format(str(subject._state[Thumb])))
         print("Palm state has changed to: {}".format(str(subject._state[Palm])))
         print("Side state has changed to: {}".format(str(subject._state[Side])))
         print("Send motor command according to this info ****  \n")
+        if self.flag_change:
+            self.move_motor_to_goal(self.close)
+        else:
+            self.move_motor_to_goal(self.open)
+        self.flag_change = not self.flag_change
+
+
+    def setup_motor(self):
+        # Initialize PortHandler and PacketHandler instance
+        portHandler = PortHandler(DEVICENAME)
+        packetHandler = PacketHandler(PROTOCOL_VERSION)
+
+        # Open port
+        if portHandler.openPort():
+            print("Succeeded to open the port")
+        else:
+            print("Failed to open the port")
+            print("Press any key to terminate...")
+            getch()
+            quit()
+
+        # Set port baudrate
+        if portHandler.setBaudRate(BAUDRATE):
+            print("Succeeded to change the baudrate")
+        else:
+            print("Failed to change the baudrate")
+            print("Press any key to terminate...")
+            getch()
+            quit()
+
+        # Enable Dynamixel Torque
+        dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_TORQUE_ENABLE, TORQUE_ENABLE)
+        if dxl_comm_result != COMM_SUCCESS:
+            print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+            print("SUCCESS")
+        elif dxl_error != 0:
+            print("%s" % packetHandler.getRxPacketError(dxl_error))
+        else:
+            print("Dynamixel has been successfully connected")
+        # Acceleration and velocity user friendly profile
+        dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, ID, ADDR_PROFILE_ACCELERATION, 3)
+        if dxl_comm_result != COMM_SUCCESS:
+            print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+        elif dxl_error != 0:
+            print("%s" % packetHandler.getRxPacketError(dxl_error))
+        dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, ID, ADDR_PROFILE_VELOCITY, 30)
+        if dxl_comm_result != COMM_SUCCESS:
+            print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+        elif dxl_error != 0:
+            print("%s" % packetHandler.getRxPacketError(dxl_error))
+
+        return portHandler, packetHandler
+
+    def cleanup_motor(self):
+        # Disable Dynamixel Torque
+        dxl_comm_result, dxl_error = self.packetHandler.write1ByteTxRx(self.portHandler, DXL_ID, ADDR_TORQUE_ENABLE, TORQUE_DISABLE)
+        if dxl_comm_result != COMM_SUCCESS:
+            print("%s" % self.packetHandler.getTxRxResult(dxl_comm_result))
+        elif dxl_error != 0:
+            print("%s" % self.packetHandler.getRxPacketError(dxl_error))
+
+        # Close port
+        self.portHandler.closePort()
+
+    def move_motor_to_goal(self, goal):
+        # Write goal position
+        dxl_comm_result, dxl_error = self.packetHandler.write4ByteTxRx(self.portHandler, DXL_ID, ADDR_GOAL_POSITION, goal)
+        if dxl_comm_result != COMM_SUCCESS:
+            print("%s" % self.packetHandler.getTxRxResult(dxl_comm_result))
+        elif dxl_error != 0:
+            print("%s" % self.packetHandler.getRxPacketError(dxl_error))
+
+        while 1:
+            # Read present position
+            dxl_present_position, dxl_comm_result, dxl_error = self.packetHandler.read4ByteTxRx(self.portHandler, DXL_ID,
+                                                                                           ADDR_PRESENT_POSITION)
+            if dxl_comm_result != COMM_SUCCESS:
+                print("%s" % self.packetHandler.getTxRxResult(dxl_comm_result))
+            elif dxl_error != 0:
+                print("%s" % self.packetHandler.getRxPacketError(dxl_error))
+
+            print("[ID:%03d] GoalPos:%03d  PresPos:%03d" % (DXL_ID, dxl_goal_position[index], dxl_present_position))
+
+            if abs(goal - dxl_present_position) <= DXL_MOVING_STATUS_THRESHOLD:
+                break
+        return
 
 
 if __name__ == "__main__":
