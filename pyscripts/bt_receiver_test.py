6b8e1f1818edba2833a5538878fcf54342109821
apequeno <alejapeq@gmail.com>
2024-06-19 16:50:33 +0200

    Bluetooth receiver test

---
+++ pyscripts/bt_receiver_test.py
@@ -0,0 +1,14 @@
+import bluetooth
+bt_add_sensor = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"
+port = 3
+sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+sock.connect((bt_add_sensor,port))
+print("Connected")
+while True:
+    try:
+        data = input()
+        sock.send(data.encode())
+        if data == 'CLOSE':
+            break
+    except KeyboardInterrupt:
+        sock.close()
