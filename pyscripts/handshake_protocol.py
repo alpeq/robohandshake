69e269094196d8b80b013470be4ef68e91e1cb3e
apequeno <alejapeq@gmail.com>
2024-07-11 11:47:58 +0200

    motor tune to move

--- pyscripts/handshake_protocol.py
+++ pyscripts/handshake_protocol.py
@@ -62,7 +62,7 @@ def shaking_phase(handler, tactile=False):
             sign_amp = -1
 
         amplitude = sign_amp * Elbow_max_amplitude * math.exp(-n/4)
-        handler.move_motors_to_goals_list([elbow_motor, wrist_motor], [int(Elbow_mean+amplitude), int(Wrist_neutral-amplitude)])
+        handler.move_motors_to_goals_list([elbow_motor, wrist_motor], [int(Elbow_mean+amplitude), int(Wrist_neutral+amplitude)])
         #handler.move_motor_to_goal(elbow_motor, int(Elbow_mean+amplitude))
         #handler.move_motor_to_goal(wrist_motor, wrist_goal)
         # handler.state_sensors if any in state==2 n = n-3 with max(n,0)
