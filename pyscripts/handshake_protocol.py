20e2ba18c2db5fd1e4262a3db7ec8f851cec54b2
apequeno <alejapeq@gmail.com>
2024-07-11 23:21:35 +0200

    tuning touch

--- pyscripts/handshake_protocol.py
+++ pyscripts/handshake_protocol.py
@@ -28,7 +28,7 @@ def handshake_protocol_tactile(handmotor_sub, wait_user=False):
     #handmotor_sub.wait_til_condition([Side, Palm], [2])
     # Open hand to init
     print("RETURN: Release me, otherwise no return!")
-    handmotor_sub.wait_til_condition([Side], [0, 1, 2], debug=False)
+    handmotor_sub.wait_til_condition([Side], [0, 1, 2, 3], debug=False)
     #time.sleep(0.52)
     opengrip_2dof_thumb(handmotor_sub)
     print("RETURN: Gripper To Open Position: My pleasure!")
